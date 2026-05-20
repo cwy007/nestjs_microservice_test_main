e6eec48fbfc6f2020639b6983ee71c99383ff045
chanweiyan <chanweiyan007@gmail.com>
2026-05-20 17:55:21 +0800

    fix: init microservice client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @Inject('USER_SERVICE')
+  private readonly userClient: ClientProxy;
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('sum')
+  sum(@Query('num') num: string) {
+    const numArr = num.split(',').map(Number);
+    return this.userClient.send('sum', numArr);
   }
 }
